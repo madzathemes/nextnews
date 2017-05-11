8109a107676ea7a0f80b971d75a1bc4840dce62e
Madars Bitenieks <madza@madzathemes.com>
2017-05-11 06:50:48 +0300

    fix

--- inc/single/single-heads.php
+++ inc/single/single-heads.php
@@ -1,6 +1,39 @@
 <?php function nextnews_single_cat() {?>
+  <?php
+  $share_top = "";
+  $share_top = get_post_meta(get_the_ID(), "magazin_post_share_top", true);
 
-  <div class="single-cat-wrap"><?php echo get_the_category_list(); ?></div>
+  /* Share Meta from Magazin framework */
+  $share = get_post_meta(get_the_ID(), "magazin_share_count", true);
+  $shares = "0";
+  if (function_exists('magazin_theme_setup')) {
+    $shares = magazin_get_shares(get_the_ID());
+  }
+  if (!empty($share)){
+  	$shares = $share+$shares;
+  }
+  $shares = number_format($shares);
+  /* View Meta from Magazin framework */
+  $view = get_post_meta(get_the_ID(), "magazin_view_count", true);
+  $views = get_post_meta(get_the_ID(), "magazin_post_views_count", true);
+  $viewes = $views + "0";
+  if (!empty($view)){ $viewes = $view + $views; $viewes = number_format($viewes); }
+  ?>
+  <div class="single-cat-wrap pull-left"><?php echo get_the_category_list(); ?></div>
+  <?php if ( false == get_theme_mod( 't_p_shares', false ) ) { $t_p_shares = esc_html__("Shares", "nextnews");  } else { $t_p_shares = get_theme_mod( 't_p_shares' ); } ?>
+  <?php if ( false == get_theme_mod( 't_p_views', false ) ) { $t_p_views = esc_html__("Views", "nextnews");  } else { $t_p_views = get_theme_mod( 't_p_views' ); } ?>
+  <?php if ( false == get_theme_mod( 't_c_comments', false ) ) { $t_c_comments = esc_html__("Comments", "nextnews");  } else { $t_c_comments = get_theme_mod( 't_c_comments' ); } ?>
+
+  <?php if(function_exists('mt_header_script')) { ?>
+  <div class="post-statistic pull-left">
+    <?php if(!empty($shares)){ ?><span class="stat-shares"><?php echo esc_attr($shares); ?> <?php echo esc_html($t_p_shares); ?></span><?php } ?>
+    <?php if(!empty($viewes)){ ?><span class="stat-views"><?php echo esc_attr($viewes); ?> <?php echo esc_html($t_p_views); ?></span><?php } ?>
+  </div>
+  <?php } ?>
+  <div class="single-stat-comments">
+    <?php if (get_comments_number()!="0") { ?><span class="stat-comments"><?php echo get_comments_number(); ?> <?php echo esc_html($t_c_comments); ?></span><?php } ?>
+  </div>
+  <div class="clearfix"></div>
 
 <?php } ?>
 <?php function nextnews_single_cat_posts() {
@@ -13,34 +46,18 @@
   <h2 class="single-subtitle" itemprop="description"><?php echo get_post_meta(get_the_ID(), "magazin_subtitle", true); ?></h2>
 <?php } ?>
 <?php function nextnews_single_social() {
+
 $share_top = "";
 $share_top = get_post_meta(get_the_ID(), "magazin_post_share_top", true);
-
-/* Share Meta from Magazin framework */
-$share = get_post_meta(get_the_ID(), "magazin_share_count", true);
-$shares = "0";
-if (function_exists('magazin_theme_setup')) {
-  $shares = magazin_get_shares(get_the_ID());
-}
-if (!empty($share)){
-	$shares = $share+$shares;
-}
-$shares = number_format($shares);
-/* View Meta from Magazin framework */
-$view = get_post_meta(get_the_ID(), "magazin_view_count", true);
-$views = get_post_meta(get_the_ID(), "magazin_post_views_count", true);
-$viewes = $views + "0";
-if (!empty($view)){ $viewes = $view + $views; $viewes = number_format($viewes); }
 
 $url = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()));
 
 ?>
   <div class="after-title">
-    <div class="pull-left">
-      <div class="author-img pull-left">
-        <?php global $post; echo get_avatar( $post->post_author, 30 ); ?>
-      </div>
       <div class="author-info">
+        <div class="author-img pull-left">
+          <?php global $post; echo get_avatar( $post->post_author, 16 ); ?>
+        </div>
         <div class="mt-author-soc hidden">
           <?php $twitterHandle = get_the_author_meta('twitter');
           $facebookHandle = get_the_author_meta('facebook');
@@ -59,24 +76,13 @@ $url = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()));
           <?php if(!empty($youtubeHandle)) { ?><a class="mt-bio-youtube" href="<?php echo $youtubeHandle; ?>"></a> <?php } ?>
           <?php if(!empty($dribbbleHandle)) { ?><a class="mt-bio-dribbble" href="<?php echo $dribbbleHandle; ?>"></a> <?php } ?>
         </div>
-        <strong><?php the_author_posts_link(); ?></strong>
-        <small class="color-silver-light"><?php the_date('M d, Y'); ?></small>
+        <div class="pull-left"><?php the_author_posts_link(); ?></div>
+        <div class="color-silver-light"><?php the_date('M d, Y'); ?></div>
+
+        <div class="clearfix"></div>
       </div>
-    </div>
-    <?php if ( false == get_theme_mod( 't_p_shares', false ) ) { $t_p_shares = esc_html__("Shares", "nextnews");  } else { $t_p_shares = get_theme_mod( 't_p_shares' ); } ?>
-    <?php if ( false == get_theme_mod( 't_p_views', false ) ) { $t_p_views = esc_html__("Views", "nextnews");  } else { $t_p_views = get_theme_mod( 't_p_views' ); } ?>
-    <?php if ( false == get_theme_mod( 't_c_comments', false ) ) { $t_c_comments = esc_html__("Comments", "nextnews");  } else { $t_c_comments = get_theme_mod( 't_c_comments' ); } ?>
     <?php if ( false == get_theme_mod( 't_p_share_on_facebook', false ) ) { $t_p_share_on_facebook = esc_html__("Share on Facebook", "nextnews");  } else { $t_p_share_on_facebook = get_theme_mod( 't_p_share_on_facebook' ); } ?>
     <?php if ( false == get_theme_mod( 't_p_share_on_twitter', false ) ) { $t_p_share_on_twitter = esc_html__("Tweet on Twitter", "nextnews");  } else { $t_p_share_on_twitter = get_theme_mod( 't_p_share_on_twitter' ); } ?>
-    <?php if(function_exists('mt_header_script')) { ?>
-    <div class="post-statistic pull-left">
-      <?php if(!empty($shares)){ ?><span class="stat-shares"><?php echo esc_attr($shares); ?> <?php echo esc_html($t_p_shares); ?></span><?php } ?>
-      <?php if(!empty($viewes)){ ?><span class="stat-views"><?php echo esc_attr($viewes); ?> <?php echo esc_html($t_p_views); ?></span><?php } ?>
-    </div>
-    <?php } ?>
-    <div class="single-stat-comments">
-      <?php if (get_comments_number()!="0") { ?><span class="stat-comments"><?php echo get_comments_number(); ?> <?php echo esc_html($t_c_comments); ?></span><?php } ?>
-    </div>
       <?php if($share_top == "no"){} else if($share_top == "yes"){ ?>
     <ul class="share top">
       <li class="share-facebook"><a class="mt-radius" href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>" target="_blank"><span><?php echo esc_html($t_p_share_on_facebook); ?></span></a></li>
