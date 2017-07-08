859efb17db71d282a3ee76e26020c07fae659cc6
madzathemes <madza@madzathemes.com>
2017-07-08 04:44:51 +0300

    fix

--- inc/functions/functions-header.php
+++ inc/functions/functions-header.php
@@ -142,6 +142,7 @@ function nextnews_head_bar() { ?>
 
 	<?php if ( true == get_theme_mod( 'mt_bar_menu', true ) ) { ?>
 
+		<?php	if ( true == get_theme_mod( 'mt_menu_share', true ) ) { nextnews_header_fixed();	} ?>
 		<div class="mt-bar mt-menu-bar mt-fx-light_ mt-bar-<?php echo get_theme_mod( 'mt_bar_menu_scheme', "light" ); ?> mt-bar-table mt-menu-bar-<?php echo get_theme_mod( 'mt_bar_menu_scheme', "light" ); ?> <?php echo get_theme_mod( 'mt_bar_menu_row', "full" ); ?> <?php if( "full" == get_theme_mod( 'mt_bar_menu_row', "full" ) or "stretched" == get_theme_mod( 'mt_bar_menu_row', "full" ) ) { ?> ful <?php } ?>">
 
 			<div class="mt-bar-out">
@@ -202,8 +203,7 @@ function nextnews_head_bar() { ?>
 
 function nextnews_mobile_menu() { ?>
 
-	<?php	if ( true == get_theme_mod( 'mt_menu_share', true ) ) { nextnews_header_fixed();	} ?>
-	
+
 	<div class="mt-bar mt-bar-mobile mt-menu-bar hidden mt-bar-<?php echo get_theme_mod( 'mt_bar_menu_scheme', "light" ); ?> mt-bar-table mt-menu-bar-<?php echo get_theme_mod( 'mt_bar_menu_scheme', "light" ); ?> <?php echo get_theme_mod( 'mt_bar_menu_row', "full" ); ?> <?php if( "full" == get_theme_mod( 'mt_bar_menu_row', "full" ) or "stretched" == get_theme_mod( 'mt_bar_menu_row', "full" ) ) { ?> ful <?php } ?>">
 
 		<div class="mt-bar-out">
