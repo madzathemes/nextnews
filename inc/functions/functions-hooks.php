7f8582dafcb42fe11be645a18e2bd766dcc8c15d
madzathemes <madza@madzathemes.com>
2017-07-15 16:00:23 +0300

    fix

--- inc/functions/functions-hooks.php
+++ inc/functions/functions-hooks.php
@@ -130,9 +130,10 @@ function nextnews_css() {
 	 }
 
 	 // Background Color
-	$options['background_color_content']))
+	 $style = $options['background_color_content'];
+	 if(!empty($style)){
 	 		$custom_styles .='.post-content-wrap, .post-style-6 .post-media-wrap, .post-style-4 .post-media-wrap, .header-wrap, .footer-wrap, .mt-container-wrap, .single-top, .post-media-wrap { background-color: '. esc_attr($style) .'!important }';
-
+	 }
 
 	 // Logo Margin
 	 if(!empty($options['logo_top'])){
