c595cd7f687f51aa0f204ad1e7ec94b4b8602054
madzathemes <madza@madzathemes.com>
2017-07-15 03:59:29 +0300

    fix

--- inc/single/single-content.php
+++ inc/single/single-content.php
@@ -99,7 +99,7 @@ $share_bottom = get_post_meta(get_the_ID(), "magazin_post_share_bottom", true);
   <?php } ?>
 
   <?php if ( true == get_theme_mod( 'mt_post_prev_next_article', true ) ) { ?>
-    <ul class="nav-single">
+    <ul class="nav-single mt-radius">
       <li class="previous pull-left"><?php previous_post_link( '%link', '<span class="">' . esc_html($t_p_previous_article) . '</span><div>%title</div>' ); ?></li>
       <li class="next pull-right"><?php next_post_link( '%link', ' <span class="">' . esc_html($t_p_next_article) . '</span><div>%title</div>' ); ?></li>
       <li class="clearfix"></li>
