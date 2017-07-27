97a59988260ce4f7b75a5dee9a9db1fe473a12d0
madzathemes <madza@madzathemes.com>
2017-07-27 20:49:06 +0300

    fix

--- functions.php
+++ functions.php
@@ -87,6 +87,13 @@ function nextnews_import_files() {
             'import_notice'                => esc_html__( 'Customize this theme from Appearance/Customize', 'nextnews' ),
         ),
 				array(
+            'import_file_name'             => esc_html__( 'Demo 6', 'nextnews' ),
+            'local_import_file'            => trailingslashit( get_template_directory() ) . 'inc/import/demo6/demo.xml',
+            'local_import_widget_file'     => trailingslashit( get_template_directory() ) . 'inc/import/demo6/widgets.json',
+            'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'inc/import/demo6/customizer.dat',
+            'import_notice'                => esc_html__( 'Customize this theme from Appearance/Customize', 'nextnews' ),
+        ),
+				array(
             'import_file_name'             => esc_html__( 'Demo 7', 'nextnews' ),
             'local_import_file'            => trailingslashit( get_template_directory() ) . 'inc/import/demo7/demo.xml',
             'local_import_widget_file'     => trailingslashit( get_template_directory() ) . 'inc/import/demo7/widgets.json',
